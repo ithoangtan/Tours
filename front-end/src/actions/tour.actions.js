a42783086e01d814dabd2b7060c39ff14bdb757e
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-10 02:32:43 +0700

    template redux and directory sort

---
+++ front-end/src/actions/tour.actions.js
@@ -0,0 +1,51 @@
+import * as tourApis from "../apis/tour.api";
+import * as tourConstants from "../constants/task.module";
+
+export const fetchListTour = () => {
+   return {
+      type: tourConstants.FETCH_TOUR
+   };
+};
+
+export const fetchListTourSuccess = data => {
+   return {
+      type: tourConstants.FETCH_TOUR_SUCCESS,
+      payload: {
+         // Thường đi làm thì người ta hay gọi
+         //data gửi kèm trong actions là payload ở vị trí này
+         data
+      }
+   };
+};
+
+export const fetchListTourError = error => {
+   return {
+      type: tourConstants.FETCH_TOUR_FAILED,
+      payload: {
+         error
+      }
+   };
+};
+/**
+ * B1: fetch isTaskRequesr()
+ * B2: ResetL state tasks --> []
+ * B3: Khi API thành công thì vào then:
+ * fetchListTourSucces (data response)
+ *
+ */
+export const fetchListTourRequest = () => {
+   return dispatch => {
+      dispatch(fetchListTour()); //reset state tasks-->[]
+      tourApis
+         .getListTour()
+         .then(resp => {
+            const { data } = resp;
+            console.log("data: ", data);
+            dispatch(fetchListTourSuccess(data));
+         })
+         .catch(error => {
+            dispatch(fetchListTourError(error));
+            console.log("error: ", error);
+         });
+   };
+};
---
+++ front-end/src/components/TourList/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import TaskItem from "../TaskItem";
+
+class TourList extends Component {
+   render() {
+      return <Grid item md={4} xs={12} key={status.value}></Grid>;
+   }
+}
+export default TourList;
