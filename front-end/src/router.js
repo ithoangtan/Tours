6e58c226cf5a5087708ac1e242e3c48c4d8712e8
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-14 03:42:19 +0700

    UI SingleTour, Tour Page

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -16,7 +16,7 @@ import BlogPage from "./CONTAINERS/BlogPage/blog.container";
 import BlogSinglePage from "./CONTAINERS/BlogSinglePage/blogSingle.container";
 import ConcactPage from "./CONTAINERS/ContactPage/concact.container";
 import DestinationPage from "./CONTAINERS/DestinationPage/destination.container";
-import TourSinglePage from "./CONTAINERS/TourPage/tourSingle.page";
+import TourSinglePage from "./CONTAINERS/TourSinglePage/tourSingle.page";
 import TourBestPage from "./CONTAINERS/TourPage/tourBest.page";
 import TourPage from "./CONTAINERS/TourPage/tour.page";
 
