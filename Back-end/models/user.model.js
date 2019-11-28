35cdfe08fb0b50fbff81cec8f296bcc1a2c60fed
Hoàng Tấn <ithoangtan@gmail.com>
2019-11-29 03:03:48 +0700

    fix name file

--- Back-end/models/user.model.js
+++ Back-end/models/user.model.js
@@ -1,43 +1,58 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mysql = require("../dbconnection");
 
-const Type = Object.freeze({
-  QuanTri: 'QuanTri',
-  KhachHang: 'KhachHang'
-})
+var Account = function(account) {
+  this.name = account.name;
+  this.email = account.email;
+  this.phone = account.phone;
+  this.role = account.role;
+  this.password = account.password;
+};
+Account.getAlluser = function(result) {
+  mysql.query("Select * from kinhdoanhtourdulich.accounts ", function(
+    err,
+    res
+  ) {
+    if (err) {
+      result(null, err);
+    } else {
+      result(null, res);
+    }
+  });
+};
+Account.loginUser = function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  mysql.query(
+    "SELECT * FROM kinhdoanhtourdulich.accounts WHERE username = ?",
+    [username],
+    function(error, results, fields) {
+      if (error) {
+        res.send({
+          code: 400,
+          failed: "error ocurred"
+        });
+      } else {
+        if (results.length > 0) {
+          if (results[0].password == password) {
+            res.send({
+              code: 200,
+              success: "login sucessfull"
+            });
+          } else {
+            res.send({
+              code: 204,
+              success: "Email and password does not match"
+            });
+          }
+        } else {
+          res.send({
+            code: 204,
+            success: "Email does not exits"
+          });
+        }
+      }
+    }
+  );
+};
 
-const userSchema = new Schema({
-  TaiKhoan: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  MatKhau: {
-    type: String,
-    required: true
-  },
-  Email: {
-    type: String
-  },
-  SoDT: {
-    type: String,
-    required: true
-  },
-  HoTen: {
-    type: String,
-  },
-  MaNhom: {
-    type: String,
-  },
-  MaLoaiNguoiDung: {
-    type: String,
-    enum: Object.values(Type),
-  },
-  hidden:{ type: Boolean, default: false },
-  datecreate: { type: Date, default: Date.now },
-  dateedit: { type: Date }
-});
-Object.assign(userSchema.statics, {
-  Type,
-})
-module.exports = mongoose.model('User', userSchema);
+module.exports = Account;
