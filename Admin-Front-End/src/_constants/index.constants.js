53f2c4964a84165bf52fcac890ec20a571e67e45
Hoàng Tấn <ithoangtan@gmail.com>
2019-12-18 12:08:27 +0700

    fix toggled admin sideBar

--- Admin-Front-End/src/_constants/index.constants.js
+++ Admin-Front-End/src/_constants/index.constants.js
@@ -1,35 +1,13 @@
 export const API_ENDPOINT = "https://api-kinhdoanhtourdulich.herokuapp.com";
 
-export const CustomerArrayExternalScript = [
-   "/customer/js/jquery.min.js",
-   "/customer/js/jquery-migrate-3.0.1.min.js",
-   "/customer/js/popper.min.js",
-   // "/customer/js/bootstrap.min.js",
-   "/customer/js/jquery.easing.1.3.js",
-   "/customer/js/jquery.waypoints.min.js",
-   "/customer/js/jquery.stellar.min.js",
-   "/customer/js/owl.carousel.min.js",
-   "/customer/js/jquery.magnific-popup.min.js",
-   "/customer/js/aos.js",
-   "/customer/js/jquery.animateNumber.min.js",
-   "/customer/js/bootstrap-datepicker.js",
-   "/customer/js/scrollax.min.js",
-   // "https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false",
-   // "/customer/js/google-map.js",
-   "/customer/js/main.js",
-   "/customer/js/ithoangtan.js",
-   "http://cdn.iframe.ly/embed.js"
-   // "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",,
-];
-
 export const AdminArrayExternalScript = [
+   "/admin/vendor/jquery/jquery.min.js",
    // "/admin/vendor/bootstrap/js/bootstrap.bundle.min.js",
    // "/admin/vendor/jquery-easing/jquery.easing.min.js",
    // "/admin/js/sb-admin-2.min.js",
    "/admin/vendor/chart.js/Chart.min.js",
    "/admin/js/demo/chart-area-demo.js",
-   "/admin/js/demo/chart-pie-demo.js",
-   "/admin/vendor/jquery/jquery.min.js"
+   "/admin/js/demo/chart-pie-demo.js"
 ];
 
 export const APIImage = "https://api-kinhdoanhtourdulich.herokuapp.com";
