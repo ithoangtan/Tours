8e605191a816889a0629c7ae1967c714a8eaa737
Hoàng Tấn <ithoangtan@gmail.com>
2019-12-24 14:16:39 +0700

    forgot password API

--- Back-End/mics/mailer.gmail.js
+++ Back-End/mics/mailer.gmail.js
@@ -5,7 +5,7 @@ var transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     xoauth2: xoauth2.createXOAuth2Generator({
-      user: "itk160454@gmail.com",
+      user: process.env.MY_GMAIL,
       clientId: process.env.CLIENT_ID_GMAIL,
       clientSecret: process.env.CLIENT_SECRET_GMAIL,
       refreshToken: process.env.REFRESH_TOKEN_GMAIL
