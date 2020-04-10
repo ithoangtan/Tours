b3f9173f57cca4a9388c5108f301233d546e17cd
Hoàng Tấn <ithoangtan@gmail.com>
2020-04-10 21:42:28 +0700

    API noticeto

--- Admin-Front-End/src/AdminParentContainer/NotificationScroll.js
+++ Admin-Front-End/src/AdminParentContainer/NotificationScroll.js
@@ -8,12 +8,18 @@ import InfiniteScroll from "react-infinite-scroller";
 import { API_ENDPOINT } from "../_constants/index.constants";
 import moment from "moment";
 
+import { getParamTokenWithName } from "../_commons/auth.service";
+import { Link } from "react-router-dom";
+
 const _LIMIT = 10;
 
 function getCookie(name) {
    const token = Cookies.get(name);
    return token;
 }
+
+const idAccount = getParamTokenWithName("idAccount");
+
 export default class NoticeScroll extends React.Component {
    state = {
       data: [],
@@ -150,95 +156,107 @@ export default class NoticeScroll extends React.Component {
                </Tooltip>
             </div>
             <div className="demo-infinite-container mt-1">
-               <InfiniteScroll
-                  initialLoad={false}
-                  pageStart={0}
-                  loadMore={this.handleInfiniteOnLoad}
-                  hasMore={!this.state.loading && this.state.hasMore}
-                  useWindow={false}
-               >
-                  <div className="ht-notification-item-container">
-                     <div className="ht-chanel-list">
-                        {/* <div className="ht-chanel">Mới</div> */}
-                        <List
-                           dataSource={this.state.data}
-                           renderItem={(item) => (
-                              <List.Item
-                                 key={item.id}
-                                 className="ht-no-boder pb-1 pt-1"
-                              >
-                                 <div className="ht-d-flex-row-space-between-baseline col-md-12">
-                                    {this.renderStyleNotification(item.type)}
-                                    <div
-                                       className={
-                                          item.status === "read"
-                                             ? "ht-d-flex-col-start-start col-md ht-title-and-date"
-                                             : "ht-d-flex-col-start-start col-md ht-title-and-date"
-                                       }
-                                    >
+               {idAccount > 0 ? (
+                  <InfiniteScroll
+                     initialLoad={false}
+                     pageStart={0}
+                     loadMore={this.handleInfiniteOnLoad}
+                     hasMore={!this.state.loading && this.state.hasMore}
+                     useWindow={false}
+                  >
+                     <div className="ht-notification-item-container">
+                        <div className="ht-chanel-list">
+                           {/* <div className="ht-chanel">Mới</div> */}
+                           <List
+                              dataSource={this.state.data}
+                              renderItem={(item) => (
+                                 <List.Item
+                                    key={item.id}
+                                    className="ht-no-boder pb-1 pt-1"
+                                 >
+                                    <div className="ht-d-flex-row-space-between-baseline col-md-12">
+                                       {this.renderStyleNotification(item.type)}
                                        <div
                                           className={
                                              item.status === "read"
-                                                ? `ht-info-main-read`
-                                                : `ht-info-main`
+                                                ? "ht-d-flex-col-start-start col-md ht-title-and-date"
+                                                : "ht-d-flex-col-start-start col-md ht-title-and-date"
                                           }
                                        >
-                                          <strong className="mr-1">
-                                             {item.title}
-                                          </strong>
-                                          {item.contentNotification}
+                                          <div
+                                             className={
+                                                item.status === "read"
+                                                   ? `ht-info-main-read`
+                                                   : `ht-info-main`
+                                             }
+                                          >
+                                             <strong className="mr-1">
+                                                {item.title}
+                                             </strong>
+                                             {item.contentNotification}
+                                          </div>
+                                          <div
+                                             className={
+                                                item.status === "read"
+                                                   ? "ht-date-read"
+                                                   : "ht-date"
+                                             }
+                                          >
+                                             {moment(item.dateTime).format(
+                                                "HH:mm A DD/MM/YYYY"
+                                             )}{" "}
+                                             {moment(item.dateTime).fromNow()}
+                                          </div>
                                        </div>
-                                       <div
-                                          className={
+                                       <Tooltip
+                                          title={
                                              item.status === "read"
-                                                ? "ht-date-read"
-                                                : "ht-date"
+                                                ? ""
+                                                : "Đánh dấu đã đọc"
                                           }
                                        >
-                                          {moment(item.dateTime).format(
-                                             "HH:mm A DD/MM/YYYY"
-                                          )}{" "}
-                                          {moment(item.dateTime).fromNow()}
-                                       </div>
+                                          <div
+                                             to="ithoangtan"
+                                             className="ht-was-read ht-d-flex-center-center ht-actions-read-and-unread"
+                                             style={{
+                                                color: this.styleType(item.type)
+                                                   .color,
+                                             }}
+                                             onClick={(event) =>
+                                                this.onReadAndUnread(
+                                                   event,
+                                                   item
+                                                )
+                                             }
+                                          >
+                                             {item.status === "read" ? (
+                                                <i className="far fa-check-circle ht-read"></i>
+                                             ) : (
+                                                <i className="fas fa-circle ht-unread"></i>
+                                             )}
+                                          </div>
+                                       </Tooltip>
                                     </div>
-                                    <Tooltip
-                                       title={
-                                          item.status === "read"
-                                             ? ""
-                                             : "Đánh dấu đã đọc"
-                                       }
-                                    >
-                                       <div
-                                          to="ithoangtan"
-                                          className="ht-was-read ht-d-flex-center-center ht-actions-read-and-unread"
-                                          style={{
-                                             color: this.styleType(item.type)
-                                                .color,
-                                          }}
-                                          onClick={(event) =>
-                                             this.onReadAndUnread(event, item)
-                                          }
-                                       >
-                                          {item.status === "read" ? (
-                                             <i className="far fa-check-circle ht-read"></i>
-                                          ) : (
-                                             <i className="fas fa-circle ht-unread"></i>
-                                          )}
-                                       </div>
-                                    </Tooltip>
+                                 </List.Item>
+                              )}
+                           >
+                              {this.state.loading && this.state.hasMore && (
+                                 <div className="demo-loading-container">
+                                    <Spin tip="Loading..." />
                                  </div>
-                              </List.Item>
-                           )}
-                        >
-                           {this.state.loading && this.state.hasMore && (
-                              <div className="demo-loading-container">
-                                 <Spin tip="Loading..." />
-                              </div>
-                           )}
-                        </List>
+                              )}
+                           </List>
+                        </div>
                      </div>
+                  </InfiniteScroll>
+               ) : (
+                  <div className="ht-no-account ht-d-flex-center-center">
+                     Vui lòng đăng nhập để sử dụng chức năng này
+                     <Link to="/login">
+                        <Button>Đăng nhập</Button>
+                     </Link>
                   </div>
-               </InfiniteScroll>
+               )}
             </div>
          </div>
       );
