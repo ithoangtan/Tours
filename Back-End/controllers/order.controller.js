992f786b8a35f6659a35228b39f6b3effa96e49d
Hoàng Tấn <ithoangtan@gmail.com>
2020-05-20 15:52:36 +0700

    Link Payment MoMo

--- Back-End/controllers/order.controller.js
+++ Back-End/controllers/order.controller.js
@@ -1,6 +1,7 @@
 const MD5 = require("md5");
-
+const crypto = require("crypto");
 const Order = require("../models/order.model");
+const reqwest = require("reqwest");
 
 exports.listAll = function (req, res) {
   //Nên dùng express-validator để validator dữ liệu trước
@@ -110,7 +111,7 @@ exports.getLinkPayment = function (req, res) {
   // Thông tin tour req.body.tour
   // Thông tin khách hàng: req.body.order
   // Tạo order
-  let newOrder = new Order(req.body.order);
+  let newOrder = req.body.order;
   let tour = req.body.tour;
   const officalPrice = tour.price * (1 - tour.sale * 0.01); // tính giá tiền đã sale
   newOrder.totalPrice =
@@ -126,48 +127,48 @@ exports.getLinkPayment = function (req, res) {
     const address = `${arrayAddress[0]}, ${arrayAddress[1]}, ${arrayAddress[2]}`;
     let secureCode = MD5(
       `${process.env.MERCHANT_SITE_CODE} ${process.env.RETURN_URL} ${
-      process.env.RECEIVER
+        process.env.RECEIVER
       } ${process.env.TRANSACTION_INFO} ${newOrder.PIN} ${
-      newOrder.totalPrice
+        newOrder.totalPrice
       } vnd 1 0 0 0 0 Tour:${tour.titleTour.trim().replace(/\s/g, "")}People-${
-      newOrder.numberPeople
+        newOrder.numberPeople
       }Children-${newOrder.numberChildren} ${newOrder.buyer
         .trim()
         .replace(/\s/g, "")}*|*${newOrder.email
-          .trim()
-          .replace(/\s/g, "")}*|*${newOrder.phone
-            .trim()
-            .replace(/\s/g, "")}*|*${address.trim().replace(/\s/g, "")}  ${
-      process.env.SECURE_PASS_SANDBOX
+        .trim()
+        .replace(/\s/g, "")}*|*${newOrder.phone
+        .trim()
+        .replace(/\s/g, "")}*|*${address.trim().replace(/\s/g, "")}  ${
+        process.env.SECURE_PASS_SANDBOX
       }`
     );
     //Tính toán link: để trả về cho trên kia
     const link = `${process.env.ENVIROMENT_NGANLUONG}?merchant_site_code=${
       process.env.MERCHANT_SITE_CODE
-      }&return_url=${process.env.RETURN_URL}&receiver=${
+    }&return_url=${process.env.RETURN_URL}&receiver=${
       process.env.RECEIVER
-      }&transaction_info=${process.env.TRANSACTION_INFO}&order_code=${
+    }&transaction_info=${process.env.TRANSACTION_INFO}&order_code=${
       newOrder.PIN
-      }&price=${
+    }&price=${
       newOrder.totalPrice
-      }&currency=vnd&quantity=1&tax=0&discount=0&fee_cal=0&fee_shipping=0&order_description=Tour:${tour.titleTour
-        .trim()
-        .replace(/\s/g, "")}People-${newOrder.numberPeople}Children-${
+    }&currency=vnd&quantity=1&tax=0&discount=0&fee_cal=0&fee_shipping=0&order_description=Tour:${tour.titleTour
+      .trim()
+      .replace(/\s/g, "")}People-${newOrder.numberPeople}Children-${
       newOrder.numberChildren
-      }&buyer_info=${newOrder.buyer
-        .trim()
-        .replace(/\s/g, "")}*|*${newOrder.email
-          .trim()
-          .replace(/\s/g, "")}*|*${newOrder.phone
-            .trim()
-            .replace(/\s/g, "")}*|*${address
-              .trim()
-              .replace(
-                /\s/g,
-                ""
-              )}&affiliate_code=&lang=vi&secure_code=${secureCode}&cancel_url=${
+    }&buyer_info=${newOrder.buyer
+      .trim()
+      .replace(/\s/g, "")}*|*${newOrder.email
+      .trim()
+      .replace(/\s/g, "")}*|*${newOrder.phone
+      .trim()
+      .replace(/\s/g, "")}*|*${address
+      .trim()
+      .replace(
+        /\s/g,
+        ""
+      )}&affiliate_code=&lang=vi&secure_code=${secureCode}&cancel_url=${
       process.env.CANCEL_URL
-      }/${tour.idTour}`;
+    }/${tour.idTour}`;
 
     // Trả về: tour, order(gồm mã PIN và tổng tiền, người mua tour), link(theo format đã cho trước),
     //  message đã sẵn sàng thanh toán!!!!
@@ -208,73 +209,84 @@ exports.notifyPayment = function (req, res) {
 };
 
 exports.getLinkMoMo = function (req, res) {
-  // let newOrder = new Order(req.body.order);
-  // let tour = req.body.tour;
-  // newOrder.totalPrice =
-  //   newOrder.numberPeople * newOrder.totalPrice +
-  //   newOrder.numberChildren * newOrder.totalPrice * 0.6;
-  // console.log(newOrder);
+  // Nhận các tham số:
+  // Thông tin tour req.body.tour
+  // Thông tin khách hàng: req.body.order
+  // Tạo order
+  let newOrder = req.body.order;
+  let tour = req.body.tour;
 
-  // Order.createOrder(newOrder, function (err, order) {
-  //   if (err) res.send(err);
-  //   // Tính tổng price dựa trên số người lớn và trẻ nhỏ (giá bằng 60% người lớn)
+  const officalPrice = tour.price * (1 - tour.sale * 0.01); // tính giá tiền đã sale
+  newOrder.totalPrice =
+    newOrder.numberPeople * officalPrice +
+    newOrder.numberChildren * officalPrice * 0.5;
 
-  //   const arrayAddress = JSON.parse(newOrder.address);
-  //   const address = `${arrayAddress[0]}, ${arrayAddress[1]}, ${arrayAddress[2]}`;
-  //   let secureCode = MD5(
-  //     `${process.env.MERCHANT_SITE_CODE} ${process.env.RETURN_URL} ${
-  //     process.env.RECEIVER
-  //     } ${process.env.TRANSACTION_INFO} ${newOrder.PIN} ${
-  //     newOrder.totalPrice
-  //     } vnd 1 0 0 0 0 Tour:${tour.titleTour.trim().replace(/\s/g, "")}People-${
-  //     newOrder.numberPeople
-  //     }Children-${newOrder.numberChildren} ${newOrder.buyer
-  //       .trim()
-  //       .replace(/\s/g, "")}*|*${newOrder.email
-  //         .trim()
-  //         .replace(/\s/g, "")}*|*${newOrder.phone
-  //           .trim()
-  //           .replace(/\s/g, "")}*|*${address.trim().replace(/\s/g, "")}  ${
-  //     process.env.SECURE_PASS_SANDBOX
-  //     }`
-  //   );
-  //   //Tính toán link: để trả về cho trên kia
-  //   const link = `${process.env.ENVIROMENT_NGANLUONG}?merchant_site_code=${
-  //     process.env.MERCHANT_SITE_CODE
-  //     }&return_url=${process.env.RETURN_URL}&receiver=${
-  //     process.env.RECEIVER
-  //     }&transaction_info=${process.env.TRANSACTION_INFO}&order_code=${
-  //     newOrder.PIN
-  //     }&price=${
-  //     newOrder.totalPrice
-  //     }&currency=vnd&quantity=1&tax=0&discount=0&fee_cal=0&fee_shipping=0&order_description=Tour:${tour.titleTour
-  //       .trim()
-  //       .replace(/\s/g, "")}People-${newOrder.numberPeople}Children-${
-  //     newOrder.numberChildren
-  //     }&buyer_info=${newOrder.buyer
-  //       .trim()
-  //       .replace(/\s/g, "")}*|*${newOrder.email
-  //         .trim()
-  //         .replace(/\s/g, "")}*|*${newOrder.phone
-  //           .trim()
-  //           .replace(/\s/g, "")}*|*${address
-  //             .trim()
-  //             .replace(
-  //               /\s/g,
-  //               ""
-  //             )}&affiliate_code=&lang=vi&secure_code=${secureCode}&cancel_url=${
-  //     process.env.CANCEL_URL
-  //     }/${tour.idTour}`;
+  let MoMoRequest = {
+    partnerCode: "MOMONGVH20200520", //need env
+    accessKey: "fPse3OVp3vTOgwaZ", //need env
+    requestId: newOrder.PIN.toString(),
+    amount: newOrder.totalPrice.toString(),
+    orderId: newOrder.PIN.toString(),
+    orderInfo: newOrder.email.toString(),
+    returnUrl: "https://kinhdoanhtourdulich.herokuapp.com/success-momo/", //need env
+    notifyUrl: "https://kinhdoanhtourdulich.herokuapp.com/notify-momo/", //need env
+    extraData: "",
+    requestType: "captureMoMoWallet", //need env
+    signature: "",
+  };
 
-  //   // Trả về: tour, order(gồm mã PIN và tổng tiền, người mua tour), link(theo format đã cho trước),
-  //   //  message đã sẵn sàng thanh toán!!!!
-  //   const message = "Hóa đơn đã sẵn sàng để thanh toán!";
-  //   const infoPayment = {
-  //     order: { ...newOrder },
-  //     tour: { ...tour },
-  //     link: link,
-  //     message: message
-  //   };
-  //   res.json(infoPayment);
-  // });
+  var rawSignature =
+    "partnerCode=" +
+    MoMoRequest.partnerCode +
+    "&accessKey=" +
+    MoMoRequest.accessKey +
+    "&requestId=" +
+    MoMoRequest.requestId +
+    "&amount=" +
+    MoMoRequest.amount +
+    "&orderId=" +
+    MoMoRequest.orderId +
+    "&orderInfo=" +
+    MoMoRequest.orderInfo +
+    "&returnUrl=" +
+    MoMoRequest.returnUrl +
+    "&notifyUrl=" +
+    MoMoRequest.notifyUrl +
+    "&extraData=" +
+    MoMoRequest.extraData;
+  var signature = crypto
+    .createHmac("sha256", "ywKiWRKcHYGwMoNhtjCtrJpiUXZ68hv7") // ywKiWRKcHYGwMoNhtjCtrJpiUXZ68hv7 = secret key nên là biến môi trường (env)
+    .update(rawSignature)
+    .digest("hex");
+  MoMoRequest.signature = signature;
+
+  const API_ENDPOINT_MOMO =
+    "https://test-payment.momo.vn/gw_payment/transactionProcessor"; //need env
+
+  reqwest({
+    url: API_ENDPOINT_MOMO,
+    method: "POST",
+    data: JSON.stringify(MoMoRequest),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  }).then((data) => {
+    // Order.createOrder(newOrder, function (err, order) {
+    // if (err) res.send(err);
+    // Tính tổng price dựa trên số người lớn và trẻ nhỏ (giá bằng 60% người lớn)
+
+    // Trả về: tour, order(gồm mã PIN và tổng tiền, người mua tour), link(theo format đã cho trước),
+    //  message đã sẵn sàng thanh toán!!!!
+    const message = "Hóa đơn đã sẵn sàng để thanh toán!";
+    const infoPayment = {
+      order: { ...newOrder },
+      tour: { ...tour },
+      // link: link,
+      message: message,
+      data,
+    };
+    res.json(infoPayment);
+    // });
+    // });
+  });
 };
