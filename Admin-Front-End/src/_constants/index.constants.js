e5270316a43c47bdb508b103bc872d79ed461467
HoangVy <hoangvynguyen42@gmail.com>
2020-05-20 09:17:17 +0700

    Update API endpoint

--- Admin-Front-End/src/_constants/index.constants.js
+++ Admin-Front-End/src/_constants/index.constants.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Spin } from "antd";
 
 require("dotenv").config();
-export const API_ENDPOINT =
-   "http://ec2-3-1-103-217.ap-southeast-1.compute.amazonaws.com:8000";
+export const API_ENDPOINT = "https://ithoangtan.online";
 // export const API_ENDPOINT = process.env.API_ENDPOINT;
 // export const API_ENDPOINT = "http://localhost:8000";
 // export const API_ENDPOINT = "https://api-kinhdoanhtourdulich.herokuapp.com";
